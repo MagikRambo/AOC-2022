5e2e55a6d61fcd511c5924326fa5818d83201175
Mike Marambio <mikemarambio@mikes-mbp-2.uconnect.utah.edu>
2022-12-03 22:49:19 -0700

    day 4 optimized

--- d4.py
+++ d4.py
@@ -2,25 +2,30 @@
 
 with open('./2022/day4/input.txt') as f:
     lines = f.read().strip().split('\n')
-    # lines = f.readlines().Split('\n')
-tot = 0
-for x in lines:
     
-    l,r = x.split(',')
-    # l1,l2,r1,r2 = map(int, list(x.split(',')).split('-'))
+def get_arrs(line:str) -> tuple:
+    l,r = line.split(',')
     l1,l2 = l.split('-')
     r1,r2 = r.split('-')
     arr1 = set(range(int(l1),int(l2)+1))
     arr2 = set(range(int(r1),int(r2)+1))
-    
+    return (arr1,arr2)
 
-    # if arr1.issubset(arr2) or arr2.issubset(arr1):
-    #     tot = tot + 1
-    if arr1 & arr2:
-        tot = tot + 1
-print(tot)
 
-   # x = l.split(',')
-    
-    #print(x)
-#print(lines)
+def part1():
+    tot = 0
+    for x in lines:
+        arr1,arr2 = get_arrs(x)
+        if arr1.issubset(arr2) or arr2.issubset(arr1):
+            tot = tot + 1
+    return tot
+def part2():
+    tot = 0
+    for x in lines: 
+        arr1,arr2 = get_arrs(x)
+        if arr1 & arr2:
+            tot = tot + 1
+    return tot
+
+print(part1())
+print(part2())
